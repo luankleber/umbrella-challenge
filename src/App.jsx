3d6a7dfdd2ac3e03972da00f347d5d745df73b55
luankleber <luankleber.sp@outlook.com>
2022-02-11 01:58:20 -0300

    Adds suggestions based on weather condition

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,23 @@ import {
   Grid,
   Heading,
   HStack,
+  Stack,
   Text,
   Box,
   Spacer,
   Image,
-  Stack,
+  Center,
+  Link,
+  LinkBox,
+  LinkOverlay,
+  Icon,
 } from "@chakra-ui/react";
 import { InputComponent } from "./components/InputComponent";
 import { useState } from "react";
 import { DateComponent } from "./components/DateComponent.jsx";
 import { FiMapPin } from "react-icons/fi";
+import { AiFillAmazonCircle } from "react-icons/ai";
+import { SiAirbnb } from "react-icons/si";
 
 function App() {
   const [tempAPI, setTempAPI] = useState("0");
@@ -25,6 +32,7 @@ function App() {
   const [humidityAPI, setHumidityAPI] = useState("0");
   const [windAPI, setWindAPI] = useState("0");
   const [pressureAPI, setPressureAPI] = useState("0");
+  const [numberAPI, setNumberAPI] = useState("");
 
   function dataAPI(weatherData) {
     setTempAPI(parseInt(weatherData.main.temp));
@@ -35,7 +43,86 @@ function App() {
     setHumidityAPI(weatherData.main.humidity);
     setWindAPI(weatherData.wind.speed);
     setPressureAPI(weatherData.main.pressure);
-    console.log(weatherData);
+    setNumberAPI(weatherData.weather[0].id);
+  }
+
+  function suggestions() {
+    var numberID = numberAPI;
+    numberID /= 100;
+    if (parseInt(numberID) === 2) {
+      return (
+        <Text as="abbr" fontSize="20px">
+          It's not a good weather outside. Try to keep yourself in a safe and
+          warm place.
+        </Text>
+      );
+    } else if (parseInt(numberID) === 3) {
+      return (
+        <Stack>
+          <Text as="abbr" fontSize="20px">
+            It's drizzling outside, maybe you need an umbrella.
+          </Text>
+          <Center bg="blue.600" h="30px" w="160px" borderRadius="8px">
+            <Text>
+              <Link
+                href="https://www.amazon.com/umbrella/s?k=umbrella"
+                isExternal
+              >
+                Buy one here.
+              </Link>
+            </Text>
+            <Icon as={AiFillAmazonCircle} marginLeft="5px" />
+          </Center>
+        </Stack>
+      );
+    } else if (parseInt(numberID) === 5) {
+      return (
+        <Stack>
+          <Text as="abbr" fontSize="20px">
+            It's raining outside, take an umbrella if you're going out.
+          </Text>
+          <Center bg="blue.600" h="30px" w="160px" borderRadius="8px">
+            <Text>
+              <Link
+                href="https://www.amazon.com/umbrella/s?k=umbrella"
+                isExternal
+              >
+                Buy one here.
+              </Link>
+            </Text>
+            <Icon as={AiFillAmazonCircle} marginLeft="5px" />
+          </Center>
+        </Stack>
+      );
+    } else if (parseInt(numberID) === 6) {
+      return (
+        <Text as="abbr" fontSize="20px">
+          It's snowing outside, don't forget your scarf and gloves.
+        </Text>
+      );
+    } else if (parseInt(numberID) === 8) {
+      return (
+        <Stack>
+          <Text as="abbr" fontSize="20px">
+            The weather is perfect for walking, traveling or discovering new
+            things.
+          </Text>
+          <Center bg="blue.600" h="30px" w="250px" borderRadius="8px">
+            <HStack>
+              <Text>
+                <Link
+                  href={`https://www.airbnb.com.br/s/all?query=${nameAPI}`}
+                  isExternal
+                >
+                  Explore {nameAPI}
+                </Link>
+              </Text>
+              <Icon as={SiAirbnb} marginLeft="5px" />
+            </HStack>
+          </Center>
+        </Stack>
+      );
+    }
   }
 
   return (
@@ -69,7 +156,9 @@ function App() {
             {nameAPI}, {countryAPI}
           </Text>
         </HStack>
-        <Box height="200px"></Box>
+        <Box paddingTop="40px" textAlign="left" height="167px">
+          {suggestions()}
+        </Box>
         <HStack height="110px" paddingTop="20px" textAlign="revert">
           <Heading fontSize="110px">{tempAPI}</Heading>
           <Text fontSize="110px">°C</Text>
@@ -105,20 +194,11 @@ function App() {
           padding="10px"
           backgroundColor="gray.800"
         >
-          <HStack paddingTop="5px">
-            <Heading textTransform="uppercase" size="md">
-              Wind Speed
-            </Heading>
-            <Spacer />
-            <Text as="abbr" fontSize="20px">
-              {windAPI} m/s
-            </Text>
-          </HStack>
           <HStack textTransform="uppercase">
             <Heading size="md">Humidity</Heading>
             <Spacer />
             <Text as="abbr" fontSize="20px">
-              {humidityAPI}%
+              {humidityAPI} %
             </Text>
           </HStack>
           <HStack>
@@ -128,6 +208,15 @@ function App() {
             <Spacer />
             <Text as="abbr" fontSize="20px">
               {pressureAPI} hPa
+            </Text>
+          </HStack>
+          <HStack paddingTop="5px">
+            <Heading textTransform="uppercase" size="md">
+              Wind Speed
+            </Heading>
+            <Spacer />
+            <Text as="abbr" fontSize="20px">
+              {windAPI} m/s
             </Text>
           </HStack>
         </Box>
